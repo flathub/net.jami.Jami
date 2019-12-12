8f61c2a34bb45bb584e52a5dc38346fd5689e0d1
Alexander Schlarb <alexander@ninetailed.ninja>
2019-12-12 19:47:08 +0100

    Upgrade to Jami version 20191209.1.4b48135 (it starts again!)
    With inspiration from flathub/net.jami.Jami#8.

--- build-debug.sh
+++ build-debug.sh
@@ -4,4 +4,7 @@ set -eu
 BUILD_DIR="build"
 REPO_DIR="repo"
 
+# Sanity check
+json-glib-validate *.json
+
 flatpak-builder -v --force-clean --repo "${REPO_DIR}" --keep-build-dirs --ccache "$@" "${BUILD_DIR}" net.jami.Jami.json
